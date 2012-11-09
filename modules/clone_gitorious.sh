9389aeb0cd9a17c496f0823cc9e88b2c8fc71272
Nick Jurgens <nick@njurgens.net>
2012-11-09 01:07:00 -0500

    add startup scripts for git-daemon, poller, sphinx, and stomp

--- modules/clone_gitorious.sh
+++ modules/clone_gitorious.sh
@@ -3,5 +3,4 @@ git clone git://gitorious.org/gitorious/mainline.git /var/www/gitorious
 cd /var/www/gitorious
 git submodule init
 git submodule update
-
 ln -s /var/www/gitorious/script/gitorious /usr/bin
