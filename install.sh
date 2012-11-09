76af045c2fb54da2e443a2f3138e32ebe7ecfbf0
Nick Jurgens <nick@njurgens.net>
2012-11-09 00:26:51 -0500

    Uncomment dependencies list

--- install.sh
+++ install.sh
@@ -23,14 +23,14 @@ if ! echo ${UBUNTU_VERSION} | grep --quiet "Ubuntu 12.04" ; then
 fi
 
 ## This section will check necessary 
-#DEPS=(build-essential  zlib1g-dev tcl-dev libexpat-dev libxslt1-dev 	\
-#		libcurl4-openssl-dev postfix apache2 mysql-server mysql-client 	\
-#		apg geoip-bin libgeoip1 libgeoip-dev sqlite3 libsqlite3-dev 	\
-#		imagemagick libpcre3 libpcre3-dev zlib1g zlib1g-dev libyaml-dev \
-#		libmysqlclient15-dev apache2-dev libonig-dev ruby-dev rubygems 	\
-#		libopenssl-ruby libdbd-mysql-ruby libmysql-ruby 				\
-#		libmagick++-dev zip unzip memcached git-core git-svn git-doc 	\
-#		git-cvs irb)
+DEPS=(build-essential  zlib1g-dev tcl-dev libexpat-dev libxslt1-dev 	\
+		libcurl4-openssl-dev postfix apache2 mysql-server mysql-client 	\
+		apg geoip-bin libgeoip1 libgeoip-dev sqlite3 libsqlite3-dev 	\
+		imagemagick libpcre3 libpcre3-dev zlib1g zlib1g-dev libyaml-dev \
+		libmysqlclient15-dev apache2-dev libonig-dev ruby-dev rubygems 	\
+		libopenssl-ruby libdbd-mysql-ruby libmysql-ruby 				\
+		libmagick++-dev zip unzip memcached git-core git-svn git-doc 	\
+		git-cvs irb)
 		
 MISSING_DEPS=()	
 for DEP in "${DEPS[@]}"; do
