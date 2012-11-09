b605f99202872a2be8a261015e1239b27c87f1d5
Nick Jurgens <nick@njurgens.net>
2012-11-09 00:57:41 -0500

    add symlink of executable to /usr/bin

--- modules/clone_gitorious.sh
+++ modules/clone_gitorious.sh
@@ -3,3 +3,5 @@ git clone git://gitorious.org/gitorious/mainline.git /var/www/gitorious
 cd /var/www/gitorious
 git submodule init
 git submodule update
+
+ln -s /var/www/gitorious/script/gitorious /usr/bin
