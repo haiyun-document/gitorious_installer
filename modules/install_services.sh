51ccb068571389c7261db0f41a36b75fff00c0a0
Nick Jurgens <nick@njurgens.net>
2012-11-09 01:23:00 -0500

    fix misspelling of git-ultrasphinx

--- modules/install_services.sh
+++ modules/install_services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp scripts/{git-{daemon,poller,ultraphinx},stomp} /etc/init.d/
+cp scripts/{git-{daemon,poller,ultrasphinx},stomp} /etc/init.d/
 chmod 755 /etc/init.d/{git-{daemon,poller,ultrasphinx},stomp}
 
 update-rc.d {git-{daemon,poller,ultrasphinx},stomp} defaults 
