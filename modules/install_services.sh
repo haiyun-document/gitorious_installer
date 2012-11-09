6356fb4edf061bad01c5e6dca8cbda6cf6138b97
Nick Jurgens <nick@njurgens.net>
2012-11-09 01:27:31 -0500

    changes to install_service.sh

--- modules/install_services.sh
+++ modules/install_services.sh
@@ -3,4 +3,7 @@
 cp scripts/{git-{daemon,poller,ultrasphinx},stomp} /etc/init.d/
 chmod 755 /etc/init.d/{git-{daemon,poller,ultrasphinx},stomp}
 
-update-rc.d {git-{daemon,poller,ultrasphinx},stomp} defaults 
+update-rc.d git-daemon defaults
+update-rc.d git-poller defaults
+update-rc.d git-ultrasphinx defaults
+update-rc.d stomp defaults
